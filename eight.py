a180e76d077551340d153c88f62a48d4844e3467
Shivaleela-2025 <shivaleelareshetty@gmail.com>
2025-04-09 18:25:10 +0530

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,5 @@
+#write a function find_largest(lst) that takes a list of numbers and returns the largest number
+def find_largest(lst):
+    return max(lst)
+numbers = [10,20,5,8,25,3]
+print(find_largest(numbers))
