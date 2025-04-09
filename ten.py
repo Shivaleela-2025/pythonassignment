d7676ebbe42786b8a427532e34a91a333ec08d36
Shivaleela-2025 <shivaleelareshetty@gmail.com>
2025-04-09 18:27:35 +0530

    Add files via upload

---
+++ ten.py
@@ -0,0 +1,18 @@
+#1.Write a Python program that takes a number from the user and checks whether it is prime or not?
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+try:
+    num = int(input("Enter a number: "))
+    if is_prime(num):
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
+except ValueError:
+    print("Please enter a valid integer.")
