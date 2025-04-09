7c9b4311956d53e70aaaf3a248f4a8e60cad3ce6
Shivaleela-2025 <shivaleelareshetty@gmail.com>
2025-04-09 18:31:02 +0530

    Add files via upload

---
+++ seventeen.py
@@ -0,0 +1,7 @@
+#Take three numbers from the user and print the largest one.
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+largest = max(a, b, c)
+print("The largest number is:", largest)
