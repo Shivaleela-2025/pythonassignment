ddcdf54be74d1211d06c6a66b66cf5e30f6f646a
Shivaleela-2025 <shivaleelareshetty@gmail.com>
2025-04-10 19:19:53 +0530

    Add files via upload

---
+++ twentyfour.py
@@ -0,0 +1,6 @@
+#2.Take a student’s name and grade, and print a formatted report like:
+# Student Grade Report
+student_name = input("Enter student name: ")
+grade = input("Enter final grade: ")
+
+print(f"\nStudent: {student_name}\nFinal Grade: {grade}")
