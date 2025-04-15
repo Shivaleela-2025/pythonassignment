527f185877bf8585dfe2c6bb7c9a229b507efe8a
Shivaleela-2025 <shivaleelareshetty@gmail.com>
2025-04-15 18:52:32 +0530

    Add files via upload

---
+++ thirtyseven.py
@@ -0,0 +1,6 @@
+# 4. Check for duplicate words in a sentence
+
+sentence = "the sky is blue and the grass is green"
+words = sentence.split()
+has_duplicates = len(words) != len(set(words))
+print("Has duplicates:", has_duplicates)
